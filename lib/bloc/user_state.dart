1f22bd8e14f970dc2a96bf00f81851d96082b25a
lulultfh <punyalulu05@gmail.com>
2026-04-24 10:53:08 +0700

    feat: menambahkan UserState

---
+++ lib/bloc/user_state.dart
@@ -0,0 +1,17 @@
+import 'package:sql_209/domain/entities/user_entity.dart';
+
+abstract class UserState {}
+
+class UserInitial extends UserState {}
+
+class UserLoading extends UserState {}
+
+class UserLoaded extends UserState {
+  final List<UserEntity> users;
+  UserLoaded(this.users);
+}
+
+class UserError extends UserState {
+  final String message;
+  UserError(this.message);
+}
