e98fced48aaeb0f0ddf06ddfdbcf4accc0349723
lulultfh <punyalulu05@gmail.com>
2026-04-24 15:50:27 +0700

    feat: menambahkan BlocListener untuk handling success dan error

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_intl_phone_field/flutter_intl_phone_field.dart';
 import 'package:sql_209/bloc/user_bloc.dart';
 import 'package:sql_209/bloc/user_event.dart';
+import 'package:sql_209/bloc/user_state.dart';
 import 'package:sql_209/domain/entities/user_entity.dart';
+
 class UserFormPage extends StatefulWidget {
   final UserEntity? user;
   const UserFormPage({super.key, this.user});
@@ -33,73 +35,87 @@ class _UserFormPageState extends State<UserFormPage> {
   @override
   Widget build(BuildContext context) {
     final isEdit = widget.user != null;
-    return Scaffold(
-      appBar: AppBar(title: Text(isEdit ? "Edit User" : "Tambah User")),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: "Nama Lengkap",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            IntlPhoneField(
-              decoration: const InputDecoration(
-                labelText: "No Telp",
-                border: OutlineInputBorder(),
-              ),
-              initialCountryCode: 'ID',
-              onChanged: (phone){
-                fullPhone = phone.completeNumber;
-              },
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _alamatController,
-              decoration: const InputDecoration(
-                labelText: "Alamat",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  final newUser = UserEntity(
-                    id: isEdit
-                        ? widget.user!.id
-                        : DateTime.now().millisecondsSinceEpoch.toString(),
-                    name: _nameController.text,
-                    email: _emailController.text,
-                    noTelp: fullPhone,
-                    alamat: _alamatController.text,
-                  );
-                  if (isEdit) {
-                    context.read<UserBloc>().add(UpdateUserEvent(newUser));
-                  } else {
-                    context.read<UserBloc>().add(AddUserEvent(newUser));
-                  }
-                  Navigator.pop(context);
-                },
+    return BlocListener<UserBloc, UserState>(
+      listener: (context, state) {
+        if (state is UserLoaded) {
+          Navigator.pop(context);
+        }
 
-                child: Text(isEdit ? "Simpan Perubahan" : "Simpan User Baru"),
+        if (state is UserError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.message)));
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text(isEdit ? "Edit User" : "Tambah User")),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: "Nama Lengkap",
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              IntlPhoneField(
+                decoration: const InputDecoration(
+                  labelText: "No Telp",
+                  border: OutlineInputBorder(),
+                ),
+                initialCountryCode: 'ID',
+                initialValue: widget.user?.noTelp,
+                onChanged: (phone) {
+                  fullPhone = phone.completeNumber;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _alamatController,
+                decoration: const InputDecoration(
+                  labelText: "Alamat",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    final newUser = UserEntity(
+                      id: isEdit
+                          ? widget.user!.id
+                          : DateTime.now().millisecondsSinceEpoch.toString(),
+                      name: _nameController.text,
+                      email: _emailController.text,
+                      noTelp: fullPhone,
+                      alamat: _alamatController.text,
+                    );
+                    if (isEdit) {
+                      context.read<UserBloc>().add(UpdateUserEvent(newUser));
+                    } else {
+                      context.read<UserBloc>().add(AddUserEvent(newUser));
+                    }
+                    // Navigator.pop(context);
+                  },
+
+                  child: Text(isEdit ? "Simpan Perubahan" : "Simpan User Baru"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
