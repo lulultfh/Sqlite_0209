755baed82a2d0260fcf3a2907a22954d0f64234e
lulultfh <punyalulu05@gmail.com>
2026-04-24 11:00:48 +0700

    feat: menambahkan userBloc

---
+++ lib/bloc/user_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:sql_209/bloc/user_event.dart';
+import 'package:sql_209/bloc/user_state.dart';
+import 'package:sql_209/domain/repository/user_repository.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository repository;
+
+  UserBloc(this.repository) : super(UserInitial()) {
+    on<LoadUsers>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final users = await repository.getAllUsers();
+        emit(UserLoaded(users));
+      } catch (e) {
+        emit(UserError("Gagal memuat data"));
+      }
+    });
+    on<AddUserEvent>((event, emit) async {
+      await repository.addUser(event.user);
+      add(LoadUsers());
+    });
+    on<UpdateUserEvent>((event, emit) async {
+      await repository.updateUser(event.user);
+      add(LoadUsers());
+    });
+    on<DeleteUserEvent>((event, emit) async {
+      await repository.deleteUser(event.id);
+      add(LoadUsers());
+    });
+  }
+}
