fd909a1fd867ff19c5e24bc423b2e51e71be2c1f
lulultfh <punyalulu05@gmail.com>
2026-04-24 13:50:52 +0700

    feat: menambahkan variabel noTelp dan alamat pada UserEntitiy serta menambahkannya pada constructor

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -2,6 +2,8 @@ class UserEntity {
   final String id;
   final String name;
   final String email;
+  final String noTelp;
+  final String alamat;
 
-  UserEntity({required this.id, required this.name, required this.email});
+  UserEntity({required this.id, required this.name, required this.email, required this.noTelp, required this.alamat});
 }
