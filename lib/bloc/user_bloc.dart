e20e7e16bd50adef3e3f23fbf7a6f5fc191a9e5c
lulultfh <punyalulu05@gmail.com>
2026-04-24 14:28:32 +0700

    feat: menambahkan logika validasi untuk inputan nomor telpon pada userBloc

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sql_209/bloc/user_event.dart';
 import 'package:sql_209/bloc/user_state.dart';
 import 'package:sql_209/domain/repository/user_repository.dart';
@@ -17,13 +17,36 @@ class UserBloc extends Bloc<UserEvent, UserState> {
       }
     });
     on<AddUserEvent>((event, emit) async {
+      final noTelp = event.user.noTelp;
+
+      if(!noTelp.startsWith('+62')){
+        emit(UserError("Nomor harus diawali dengan +62"));
+        return;
+      }
+      if(noTelp.length > 15){
+        emit(UserError("Nomor tidak boleh lebih dari 15 karakter"));
+        return;
+      }
+
       await repository.addUser(event.user);
       add(LoadUsers());
     });
+
     on<UpdateUserEvent>((event, emit) async {
+      final noTelp = event.user.noTelp;
+      
+      if(!noTelp.startsWith('+62')){
+        emit(UserError("Nomor harus diawali dengan +62"));
+        return;
+      }
+      if(noTelp.length > 15){
+        emit(UserError("Nomor tidak boleh lebih dari 15 karakter"));
+        return;
+      }
       await repository.updateUser(event.user);
       add(LoadUsers());
     });
+
     on<DeleteUserEvent>((event, emit) async {
       await repository.deleteUser(event.id);
       add(LoadUsers());
