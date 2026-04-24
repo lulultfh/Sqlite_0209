05b695798b5b0b2f04055bac64e5f99246ff55b4
lulultfh <punyalulu05@gmail.com>
2026-04-24 11:02:45 +0700

    feat: menambahkan class UserFormPage sebagai turunan dari StatefulWIdget

---
+++ lib/pages/user_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserFormPage extends StatefulWidget {
+  const UserFormPage({super.key});
+
+  @override
+  State<UserFormPage> createState() => _UserFormPageState();
+}
+
+class _UserFormPageState extends State<UserFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
