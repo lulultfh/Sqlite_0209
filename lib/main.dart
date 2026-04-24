17fdf8d6c6ae92ae5f073ff20d744aa46529ce42
lulultfh <punyalulu05@gmail.com>
2026-04-24 13:09:34 +0700

    feat: menambahkan inisialisasi userBloc, DatabaseHelper, dan repository, serta memanggil HomePage sebagai home

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sql_209/bloc/user_bloc.dart';
+import 'package:sql_209/bloc/user_event.dart';
+import 'package:sql_209/data/repository/user_repository_impl.dart';
+import 'package:sql_209/helper/database_helper.dart';
+import 'package:sql_209/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized;
+
+  final dbHelper = DatabaseHelper();
+  final userRepository = UserRepositoryImpl(dbHelper);
+
+  runApp(MyApp(repository: userRepository));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final UserRepositoryImpl repository;
+  const MyApp({super.key, required this.repository});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+      create: (context) => UserBloc(repository)..add(LoadUsers()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true, colorSchemeSeed: Colors.blue,
+        ),
+        home: const HomePage()
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
