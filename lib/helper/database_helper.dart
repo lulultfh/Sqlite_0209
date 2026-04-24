25c383ff3e8a3522bf612ef5b118e17da1c51aa8
lulultfh <punyalulu05@gmail.com>
2026-04-24 14:58:35 +0700

    feat: mengupdate database schema menuju version 2

--- lib/helper/database_helper.dart
+++ lib/helper/database_helper.dart
@@ -18,11 +18,17 @@ class DatabaseHelper {
     String path = join(await getDatabasesPath(), 'user_database.db');
     return await openDatabase(
       path,
-      version: 1,
+      version: 2,
       onCreate: (db, version) {
         return db.execute(
-          'CREATE TABLE users(id TEXT PRIMARY KEY, name TEXT, email TEXT, noTelp TEXT, alamat TEXT)',
+          'CREATE TABLE users(id TEXT PRIMARY KEY, name TEXT, email TEXT)',
         );
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        if (oldVersion < 2) {
+          await db.execute("ALTER TABLE users ADD COLUMN noTelp TEXT");
+          await db.execute("ALTER TABLE users ADD COLUMN alamat TEXT");
+        }
       },
     );
   }
